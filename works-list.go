3066589c0ec632d574016fd22d2f5f73505bf688
tarkanic909 <tarkanic909@gmail.com>
2022-10-31 07:22:14 +0100

    check for bad sort arg

--- works-list.go
+++ works-list.go
@@ -3,16 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	exitCode := 0
 	bookArg := flag.String("book", "Lord of the rings", "book name")
-	authorArg := flag.String("author", "asc", "sort by author name asc/desc")
-	revisionArg := flag.String("revision", "asc", "sort by count of revision asc/desc")
+	sortArg := flag.String("sort", "asc", "sort by count of revision asc/desc")
+
+	defer func() {
+		os.Exit(exitCode)
+	}()
 
 	flag.Parse()
 
+	if *sortArg != "asc" && *sortArg != "desc" {
+		fmt.Println("Bad sort argument!", "Use asc or desc!")
+		exitCode = 1
+	}
+
 	fmt.Println("book: ", *bookArg)
-	fmt.Println("author: ", *authorArg)
-	fmt.Println("revision: ", *revisionArg)
+	fmt.Println("sort: ", *sortArg)
 }
