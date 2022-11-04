5ef6d166d7974270c409bacb2fba1b01057cc0df
tarkanic909 <tarkanic909@gmail.com>
2022-11-04 13:14:38 +0100

    add getbookbyisbn functionality

--- bookService/bookService.go
+++ bookService/bookService.go
@@ -2,26 +2,35 @@ package bookService
 
 import (
 	"encoding/json"
-	"fmt"
+	"regexp"
 	"worksList/getClient"
 )
 
 const bookUrl = "https://openlibrary.org/api/books?bibkeys=ISBN:"
 
-type bookResponse struct {
-	authors []struct {
-		name string
-	}
+type BookResponse struct {
+	Book Book
 }
 
-func getBookByISBN(isbn string) {
-	var response bookResponse
-	fmt.Println("hello from book service")
+type Book struct {
+	Title   string `json:"title"`
+	Authors []struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"authors"`
+}
+
+func GetBookByISBN(isbn string) Book {
+	var response BookResponse
+	// r := regexp.MustCompile(`\w+:[0-9]\d{1,13}`)
+	r := regexp.MustCompile(`\bISBN:\d{1,13}`)
 
 	body := getClient.Get(bookUrl + isbn + "&format=json&jscmd=data")
 
-	json.Unmarshal([]byte(body), &response)
+	// replace ISBN:number with book
+	result := r.ReplaceAllString(string(body), "Book")
+	json.Unmarshal([]byte(result), &response)
 
-	// return response
+	return response.Book
 
 }
