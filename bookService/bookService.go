a2b5bf79538a29328c44fe6288649563dcd6c229
tarkanic909 <tarkanic909@gmail.com>
2022-11-07 10:19:03 +0100

    getBookByOLID function

--- bookService/bookService.go
+++ bookService/bookService.go
@@ -2,23 +2,10 @@ package bookService
 
 import (
 	"encoding/json"
-	"fmt"
 	"worksList/getClient"
 )
 
 const bookUrl = "https://openlibrary.org/books/"
-
-type BookResponse struct {
-	Book Book
-}
-
-type Book struct {
-	Title   string `json:"title"`
-	Authors []struct {
-		Name string `json:"name"`
-		Url  string `json:"url"`
-	} `json:"authors"`
-}
 
 type BookOlid struct {
 	Title   string `json:"title"`
@@ -27,14 +14,12 @@ type BookOlid struct {
 	} `json:"authors"`
 }
 
-func GetBookByOLID(olid string) Book {
+func GetBookByOLID(olid string) BookOlid {
 	var response BookOlid
 
 	body := getClient.Get(bookUrl + olid + ".json")
-	fmt.Println(string(body))
 
 	json.Unmarshal([]byte(body), &response)
-	fmt.Println(response)
 
-	return Book{Title: response.Title}
+	return response
 }
