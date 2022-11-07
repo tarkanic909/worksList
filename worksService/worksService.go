61f87f36c3547a32924eafa26a9ed90f1c704a49
tarkanic909 <tarkanic909@gmail.com>
2022-11-07 10:17:45 +0100

    add getWorks and getAll works functions

--- worksService/worksService.go
+++ worksService/worksService.go
@@ -5,7 +5,7 @@ import (
 	"worksList/getClient"
 )
 
-const url = "https://openlibrary.org/authors/"
+const url = "https://openlibrary.org/"
 
 type Work struct {
 	Title    string
@@ -14,15 +14,51 @@ type Work struct {
 
 type WorksResponse struct {
 	Entries []Work
+	Links   struct {
+		Next string
+	}
 }
 
-func GetWorks(authorKey string) []Work {
+func getWorks(next string, allWorks *[]Work) {
 	var response WorksResponse
+
+	if next != "" {
+		body := getClient.Get(url + next)
+		json.Unmarshal([]byte(body), &response)
+		*allWorks = append(*allWorks, response.Entries...)
+		getWorks(response.Links.Next, allWorks)
+	}
+
+}
+
+func GetAllWorks(authorKey string) []Work {
+	var response WorksResponse
+	var allWorks []Work
 
 	body := getClient.Get(url + authorKey + "/works.json")
 
 	json.Unmarshal([]byte(body), &response)
+	allWorks = append(allWorks, response.Entries...)
 
-	return response.Entries
+	if response.Links.Next != "" {
+		getWorks(response.Links.Next, &allWorks)
+	}
+
+	return allWorks
+
+}
+
+type Author struct {
+	Name string
+}
+
+func GetAuthorByKey(authorKey string) string {
+	var response Author
+
+	body := getClient.Get(url + authorKey + ".json")
+
+	json.Unmarshal([]byte(body), &response)
+
+	return response.Name
 
 }
